ceebf492721ac2c48d19d41d3147df2d223ed03c
RoryChou <365538079@qq.com>
2016-12-30 10:55:31 +0800

    bug fixed
    竖向和横向的bug问题解决了

--- js/test.js
+++ js/test.js
@@ -11,18 +11,19 @@ $(function () {
             swiperAnimate(swiper); //初始化完成开始动画
         },
         onSlideChangeEnd: function(swiper){
-            console.log('end1')
-            //如果index是1的话，替换swiper对象为inside对象,令swiper2的初始位置回到第一个上
+            //console.log('end1')
+            //如果index是1的话，替换swiper对象为inside对象
             if(swiper.activeIndex == 1){
                 swiper = swiper2
-                console.log(swiper2.activeIndex);
-                if(swiper2.activeIndex == 1){
-                    /*var x = swiper2.getWrapperTranslate('x')
-                    console.log(x)
-                    swiper2.activeIndex = 0;
-                    swiper2.setWrapperTranslate(x);*/
-                    swiper2.slideTo(0, 1, true);
-                }
+                //console.log(swiper2.activeIndex);
+            }
+            //如果swiper2的初始位置不在0，使它复位
+            if(swiper2.activeIndex == 1){
+                /*var x = swiper2.getWrapperTranslate('x')
+                 console.log(x)
+                 swiper2.activeIndex = 0;
+                 swiper2.setWrapperTranslate(x);*/
+                swiper2.slideTo(0, 1, false);
             }
             swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
         }
@@ -37,9 +38,9 @@ $(function () {
         },
     });
 
-    swiper2.on('click', function(swiper){
+    /*swiper2.on('click', function(swiper){
         //swiper2.setWrapperTranslate(200)
         swiper2.slideTo(0, 1, false);
         swiperAnimate(swiper);
-    });
+    });*/
 })
